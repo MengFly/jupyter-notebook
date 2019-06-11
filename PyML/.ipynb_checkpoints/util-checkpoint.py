3d9609923136dfc864f4c2a9033ac87c4d2747a2
wangpengfei <www.wangpengfei99@gmail.com>
2019-06-11 11:00:55 +0800

    update notebook

--- PyML/.ipynb_checkpoints/util-checkpoint.py
+++ PyML/.ipynb_checkpoints/util-checkpoint.py
@@ -49,3 +49,19 @@ def load_iris_data():
     # 取出我们需要的特征信息
     X = df.iloc[0:100, [0, 2]].values
     return X, y
+
+def load_std_data():
+    from sklearn import datasets
+    from sklearn.preprocessing import StandardScaler
+    from sklearn.cross_validation import train_test_split
+    iris = datasets.load_iris()
+    X = iris.data[:, [2, 3]]
+    y = iris.target
+    X_train, X_test, y_train, y_test = train_test_split(X, y,           test_size=0.3, random_state = 0)
+    sc = StandardScaler()
+    sc.fit(X_train)
+    X_train_std = sc.transform(X_train)
+    X_test_std = sc.transform(X_test)
+    return X_train_std, y_train, X_test_std, y_test
+    
+    
